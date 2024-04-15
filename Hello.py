09b346d4010099dd62747c4a90fdba40f6c218aa
Akanksha Singh <akanksha.singh0710@gmail.com>
2024-04-15 16:56:52 +0000

    Initial Commit

--- Hello.py
+++ Hello.py
@@ -1,51 +1,97 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 from streamlit.logger import get_logger
+import google.generativeai as genai
+import PIL.Image
 
 LOGGER = get_logger(__name__)
 
-
 def run():
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="Answer Grader",
+        layout='wide'
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    # Header section
+    header_right, _ = st.columns((2, 1))
+    with header_right:
+        st.title("Answer Grader")
 
-    st.sidebar.success("Select a demo above.")
+    container = st.container()
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    with container:
+        add_answer_button_clicked = True
+        isDisabled_text = False 
+        isDisabled_image = False
+        isDisabled_rubric = False
+        score = None
+    
+
+        col1, col2, col3 = st.columns([1, 1, 0.3])  # Adjusted column widths
+
+        with col1:
+            uploaded_file = st.file_uploader(f"Upload answer here:", type=['png', 'jpeg', 'jpg'], disabled=isDisabled_image)
+            if uploaded_file is not None:
+                isDisabled_text = True
+            
+            st.write("OR")
+            uploaded_text = st.text_area("paste answer here:", disabled=isDisabled_text)
+            if uploaded_text.strip() !=  "":
+                isDisabled_image = True
+
+        with col2:
+            rubric = st.text_area("Enter Rubric", key="text", height= 260)
+            
+
+        with col3:
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            grade_button = st.button(f"Grade Answer", type='primary')
+            if grade_button:
+                if rubric.strip() == "":
+                    st.warning("Please add rubric.")
+                elif uploaded_file is None:
+                    st.warning("Please add answer.")
+                else:
+                    score = grade(uploaded_file, rubric)
+
+    _ , add_answer_col, _ = st.columns([1.5,1,1])
+    with add_answer_col:
+        if st.button("Add new Answer"):
+            uploaded_file = None
+            uploaded_text = None
+            rubric = None
+            score= None
+    # Display grading result
+    if score is not None:
+        st.subheader("Grading Result:")
+        st.write(score)
+   
+
+def grade(uploaded_file, rubric):
+    img_model = genai.GenerativeModel('gemini-pro-vision')
+    img = PIL.Image.open(uploaded_file)
+    response = img_model.generate_content(["Grab the text from the image.", img], stream=True)
+    response.resolve()
+    student_answer = response.text
+
+    text_model = genai.GenerativeModel('gemini-pro')
+
+    prompt = f"""Use the provided question\'s rubric to give a score for the following answer. Also provide reason for the score.
+    <RUBRIC>: {rubric}
+    <ANSWER>: {student_answer}
+    Output should be of the format: 
+    <SCORE>: score
     """
-    )
-
+    response = text_model.generate_content([prompt], stream=True)
+    response.resolve()
+    score = response.text
+    return score
 
 if __name__ == "__main__":
     run()
