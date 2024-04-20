a91bfe2297513c2a52322b3c4257f5c592477454
Aadyant12 <62593634+Aadyant12@users.noreply.github.com>
2024-04-20 01:40:24 +0000

    marking_type

--- grader.py
+++ grader.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 from config import GENAI_API_KEY
 import PIL.Image
 
-def grader(uploaded_file, uploaded_text, rubric):
+def grader(uploaded_file, uploaded_text, rubric, marking_type):
     genai.configure(api_key=GENAI_API_KEY)
     
     if uploaded_file is not None:
@@ -20,14 +20,35 @@ def grader(uploaded_file, uploaded_text, rubric):
             temperature=0.1,
     ))
 
-    prompt = f"""Use the provided question\'s rubric to give a score for the following answer. Also provide reason for the score.
+    if marking_type == 'lenient':
+        prompt = f"""Use the provided question\'s rubric to give a score for the following answer. Do very lenient marking.
 
-    <RUBRIC>: {rubric}
-    <ANSWER>: {student_answer}
+        <RUBRIC>: {rubric}
+        <ANSWER>: {student_answer}
 
-    Output should be of the format: 
-    <SCORE>: score
-    """
+        Output should be of the format: 
+        <SCORE>: score
+        """
+    elif marking_type == 'strict':
+        prompt = f"""Use the provided question\'s rubric to give a score for the following answer. Do very strict marking.
+
+        <RUBRIC>: {rubric}
+        <ANSWER>: {student_answer}
+
+        Output should be of the format: 
+        <SCORE>: score
+        """
+    
+    else:
+        prompt = f"""Use the provided question\'s rubric to give a score for the following answer.
+
+        <RUBRIC>: {rubric}
+        <ANSWER>: {student_answer}
+
+        Output should be of the format: 
+        <SCORE>: score
+        """
+    
     response = text_model.generate_content([prompt], stream=True)
     response.resolve()
     score = response.text
